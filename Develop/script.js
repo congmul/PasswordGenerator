b0270ef6da1008ec1376b9d1a6cb53b5c00fe94c
Jehyun <congmul@hotmail.com>
2020-09-16 00:31:02 -0700

    Password Generator rev01

--- Develop/script.js
+++ Develop/script.js
@@ -11,6 +11,72 @@
 //
 // * Gather user input with prompt's and confirm's
 
+function generatePassword(){
+  var passwordLength = prompt("How long password length do you need?");
+  var isSpecialChar = confirm("Do you want to add Special Charactors?");
+  var isUpperCase = confirm("Do you want to add Upper Case letters?");
+  var isLowerCase = confirm("Do you want to add Lower Case letters?");
+  var numPassWord ="";
+  var specialCharPassWord ="";
+  var upperCasePassWord ="";
+  var lowerCasePassWord ="";
+  var totalPassWord = "";
+  var shuffledPassWord ="";
+  
+  for (let i = 0; i < passwordLength; i++){
+        numPassWord += Math.round(Math.random() * 9) + 1;
+  }
+
+  if(isSpecialChar){
+    for (let i = 0; i < passwordLength; i++){
+      specialCharPassWord += String.fromCharCode(Math.round(Math.random() * 14) + 33);  // from 33 to 47
+    }
+  }
+
+  if(isUpperCase){
+    for (let i = 0; i < passwordLength; i++){
+      upperCasePassWord += String.fromCharCode(Math.round(Math.random() * 25) + 65);  // from 65 to 90
+    }
+  }
+
+  if(isLowerCase){
+    for (let i = 0; i < passwordLength; i++){
+      lowerCasePassWord += String.fromCharCode(Math.round(Math.random() * 25) + 97);  // from 97 to 132
+    }
+  }
+
+  // Add to totalPassword
+  totalPassWord = numPassWord + lowerCasePassWord + upperCasePassWord + specialCharPassWord;
+  
+  //Debug - checking totalPassword.length
+  console.log("Length: " + totalPassWord.length);
+
+  // Shuffle 
+  var iteratorAmount = 1;
+  if(isSpecialChar || isUpperCase || isLowerCase){
+    iteratorAmount = 2;
+  }
+  if((isSpecialChar && isUpperCase) || (isSpecialChar && isLowerCase) || (isUpperCase && isLowerCase) ){
+    iteratorAmount = 3;
+  }
+  if(isSpecialChar && isUpperCase && isLowerCase){
+    iteratorAmount = 4;
+  }
+  
+  //Debug - checking iteratorAmount
+  console.log("iteratorAmount : " + iteratorAmount);
+
+  for (let i = 0; i < passwordLength; i++){
+    shuffledPassWord += totalPassWord[Math.round(Math.random() * (passwordLength * iteratorAmount))];
+    
+    //Debug - checking index number
+    console.log("Random index: "+ Math.round(Math.random() * (passwordLength * iteratorAmount)));
+  }
+  return shuffledPassWord;
+}
+
+
+
 ///////////////////////////////////////////////////////////////////////
 // DO NOT TOUCH THIS CODE
 //
